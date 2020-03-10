2232c8b70497ad91b2ca50763d2df650743fc060
crimsonpython24 <crimsonpython24@gmail.com>
2020-03-10 20:40:22 +0800

    base components

---
+++ scipj_env/project/scipj/core/src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import useNavbarStyles from '../styles/components/Navbar';
+
+export default function PersistentDrawerLeft() {
+  const classes = useNavbarStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={clsx(classes.appBar, {[classes.appBarShift]: open,})}>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer className={classes.drawer} variant="persistent" anchor="left" open={open}
+        classes={{paper: classes.drawerPaper,}}>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+      </Drawer>
+    </div>
+  );
+}
--- scipj_env/project/scipj/core/src/index/index.js
+++ scipj_env/project/scipj/core/src/index/index.js
@@ -1,23 +1,5 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import ReactDOM from 'react-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import Navbar from '../components/Navbar';
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    maxWidth: 500,
-  },
-});
-
-function Index() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
-
-ReactDOM.render(<Index />, document.querySelector('#app'));
+ReactDOM.render(<Navbar />, document.querySelector('#app'));
