1c31816554227b555b5f7153a78d4f62c68a79fe
crimsonpython24 <crimsonpython24@gmail.com>
2020-03-16 20:34:17 +0800

    simplified tabs, adding textarea

--- scipj_env/project/scipj/hash/src/sample/layout.js
+++ scipj_env/project/scipj/hash/src/sample/layout.js
@@ -19,124 +19,67 @@ function SimpleContainer() {
   const layout_classes = useLayoutStyles();
   
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
-        <Typography variant="overline" display="block" gutterBottom style={{ paddingTop: '64px', marginBottom: '0px'}}>
-          Consectetur adipiscing elit, vivamus orci velit
-        </Typography>
-        <Typography variant="h4" gutterBottom>
-          Lorem Ipsum dolor sit Amet
-        </Typography>
-        <div className={layout_classes.root }>
-          <Grid container spacing={0}>
-            <Grid item xs={12} md={5} style={{ height: "455px" }}>
-            </Grid>
-            <Grid item xs={12} md={7} style={{ height: "455px",  padding: "30px" }}>
-              <Typography variant="body1" gutterBottom>
-                Vestibulum efficitur enim sed leo vehicula, nec vulputate neque aliquam. Morbi hendrerit neque massa,
-                in finibus libero egestas vitae. Curabitur mattis orci sapien, vitae bibendum elit finibus nec.
-                Integer rutrum tellus sem, id egestas metus aliquam eget. In commodo ultricies gravida. Suspendisse mi ante,
-                hendrerit commodo lorem sit amet, finibus elementum mauris. Proin vehicula libero et elit venenatis gravida.
-              </Typography>
-            </Grid>
+    <Container maxWidth="lg" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
+      <Typography variant="overline" display="block" gutterBottom style={{ paddingTop: '64px', marginBottom: '0px'}}>
+        Consectetur adipiscing elit, vivamus orci velit
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        Lorem Ipsum dolor sit Amet
+      </Typography>
+      <div className={layout_classes.root }>
+        <Grid container spacing={0}>
+          <Grid item xs={12} md={5} style={{ height: "455px" }}>
           </Grid>
-        </div>
-      </Container>
-    </React.Fragment>
+          <Grid item xs={12} md={7} style={{ height: "455px",  padding: "30px" }}>
+            <Typography variant="body1" gutterBottom>
+              Vestibulum efficitur enim sed leo vehicula, nec vulputate neque aliquam. Morbi hendrerit neque massa,
+              in finibus libero egestas vitae. Curabitur mattis orci sapien, vitae bibendum elit finibus nec.
+              Integer rutrum tellus sem, id egestas metus aliquam eget. In commodo ultricies gravida. Suspendisse mi ante,
+              hendrerit commodo lorem sit amet, finibus elementum mauris. Proin vehicula libero et elit venenatis gravida.
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
   );
 }
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value1: PropTypes.any.isRequired,
-  value2: PropTypes.any.isRequired
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`
-  };
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  }
-}));
 
 function SimpleTabs() {
-  const classes = useStyles();
-  const [value1] = React.useState(0);
-  const [value2, setValue2] = React.useState(0);
+  const [value1, setValue1] = React.useState(2);
+  const [value2, setValue2] = React.useState(2);
 
+  const handleChange1 = (event, newValue) => {
+    setValue1(newValue);
+  };
   const handleChange2 = (event, newValue) => {
     setValue2(newValue);
   };
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={0} style={{ paddingTop: '64px', marginBottom: '0px'}}>
+    <Container maxWidth="lg" style={{ paddingLeft: '0px', paddingRight: '0px', paddingTop: '64px', marginBottom: '0px'}}>
+      <Grid container spacing={0}>
         <Grid item xs={12} sm={6}>
-          <AppBar position="static">
-            <Tabs
-              value={value1}
-              indicatorColor="primary"
-              textColor="primary"
-              aria-label="disabled tabs example"
-            >
-              <Tab label="Input Text" />
+          <Paper square>
+            <Tabs value={value1} indicatorColor="primary" textColor="primary" onChange={handleChange1} aria-label="disabled tabs example">
+              <Tab label="Active" />
+              <Tab label="Disabled"/>
+              <Tab label="Active" />
             </Tabs>
-          </AppBar>
-          <TabPanel value={value1} index={0}>
-            <Paper variant="outlined">Item One</Paper>
-          </TabPanel>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <AppBar position="static">
-            <Tabs
-              value={value2}
-              indicatorColor="primary"
-              textColor="primary"
-              onChange={handleChange2}
-              aria-label="disabled tabs example"
-            >
-              >
-              <Tab label="Item One" {...a11yProps(0)} />
-              <Tab label="Item Two" {...a11yProps(1)} />
-              <Tab label="Item Three" {...a11yProps(2)} />
+          <Paper square>
+            <Tabs value={value2} indicatorColor="primary" textColor="primary" onChange={handleChange2} aria-label="disabled tabs example">
+              <Tab label="Active" />
+              <Tab label="Disabled"/>
+              <Tab label="Active" />
             </Tabs>
-          </AppBar>
-          <TabPanel value={value2} index={0}>
-            <Paper variant="outlined">Item One</Paper>
-          </TabPanel>
-          <TabPanel value={value2} index={1}>
-            <Paper variant="outlined">Item Two</Paper>
-          </TabPanel>
+          </Paper>
         </Grid>
       </Grid>
-    </div>
+    </Container>
   );
 }
-
   
 ReactDOM.render(<div><Navbar/></div>, document.querySelector('#navbar'));
 ReactDOM.render(<div><SimpleContainer/></div>, document.querySelector('#view-1'));
