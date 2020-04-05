d0a5f5fe0d05d9ce6fa4b8f91f60f1fcd454d588
crimsonpython24 <crimsonpython24@gmail.com>
2020-04-05 18:37:30 +0800

    arrayfield

--- scipj_env/project/scipj/hash/models.py
+++ scipj_env/project/scipj/hash/models.py
@@ -1,6 +1,7 @@
 import os
 
 from django.db import models
+from django.contrib.postgres import fields
 from django.utils.translation import ugettext_lazy as _
 from django.urls import reverse
 from django.utils.timezone import now as timezone_now
@@ -23,11 +24,13 @@ class Algorithm(UrlMixin, CreationModificationDateMixin, MetaTagsMixin):
 
     slug = models.SlugField(unique=True)
     name = models.CharField(_("Name"), max_length=100)
-    subtitle = models.CharField(_("Subtitle"), max_length=200)
+    subtitle = models.CharField(_("Subtitle"), max_length=250)
     paragraph1 = models.TextField(_("Description1"), max_length=350)
     paragraph2 = models.TextField(_("Description2"), max_length=350)
     paragraph3 = models.TextField(_("Description3"), max_length=350)
     image = models.ImageField(_("Image"), upload_to=upload_to)
+    stepper_header = fields.ArrayField(models.CharField(max_length=100))
+    stepper_description = fields.ArrayField(models.CharField(max_length=300))
 
     def __str__(self):
         return self.name
